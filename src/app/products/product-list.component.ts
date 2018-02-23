bdfa213b4085e6c5a3ceee79a75fed1f700186bb
nemanjarotman <34609928+nemanjarotman@users.noreply.github.com>
2018-02-23 22:57:36 +0100

    Added 2 way data binding and fixed currency

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -6,8 +6,12 @@ import {Component} from '@angular/core';
 })
 
 export class ProductListComponent {
-    pageTitle:string='Product List';
-    products:any[]=[
+    pageTitle : string='Product List';
+    imageWidth : number = 50;
+    imageMargin : number = 2;
+    showImage : boolean = false;
+    listFilter : string='cart'
+    products : any[]=[
         {
             "productId": 2,
             "productName": "Garden Cart",
@@ -30,4 +34,8 @@ export class ProductListComponent {
         }
 
     ];
+
+    toggleImage():void{
+        this.showImage=!this.showImage;
+    }
 }
