843cad5379630adf24ee0a5c9b80c9a278db77e4
nemanjarotman <34609928+nemanjarotman@users.noreply.github.com>
2018-02-26 23:37:18 +0100

    Added HTTP for service, now read products from json file

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import { IProduct } from './product';
 import { ProductService } from './product.service';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
     selector:'pm-products',
     templateUrl:'./product-list.component.html',
@@ -13,7 +13,9 @@ export class ProductListComponent implements OnInit{
     imageWidth : number = 50;
     imageMargin : number = 2;
     showImage : boolean = false;
+    errorMessage: string;
     _listFilter : string;
+    
 
     get listFilter():string{
         return this._listFilter;
@@ -46,7 +48,12 @@ export class ProductListComponent implements OnInit{
     }
 
     ngOnInit():void{
-        this.products=this._productService.getProduct();
+        this._productService.getProduct()
+                .subscribe(products=>{
+                    this.products=products;
+                },
+                           error=>this.errorMessage=<any>error);
+
         this.filteredProducts=this.products;
     }
 }
