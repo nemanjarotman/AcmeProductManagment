b419dd0294f62ea9407b5b9bd8e89b6e4388fe0b
nemanjarotman <34609928+nemanjarotman@users.noreply.github.com>
2018-03-13 13:52:50 +0100

    Some fixes, added guards for router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { StarComponent } from './shared/star.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ProductDetailComponent } from './products/product-detail.component';
 import { WelcomeComponent } from './home/welcome.component';
+import { ProductGuardService } from './products/product-guard.service';
 
 
 @NgModule({
@@ -27,13 +28,15 @@ import { WelcomeComponent } from './home/welcome.component';
     HttpClientModule,
     RouterModule.forRoot([
       {path: 'products', component:ProductListComponent},
-      {path: 'products/:id', component:ProductDetailComponent},
+      {path: 'products/:id',
+       canActivate:[ProductGuardService],
+      component:ProductDetailComponent},
       {path: 'welcome', component:WelcomeComponent},
       {path: '',redirectTo:'welcome', pathMatch:'full'},
       {path:'**',redirectTo:'welcome', pathMatch:'full'}
     ])
   ],
-  providers: [],
+  providers: [ProductGuardService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
